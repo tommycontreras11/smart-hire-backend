3c1e41583717e553e2996b6373a38b23f4d36eae
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-05-14 21:36:17 -0400

    Add work experience creation route and refactor existing routes for clarity

--- src/routers/api/work-experience/index.ts
+++ src/routers/api/work-experience/index.ts
@@ -1,16 +1,36 @@
 import { Router } from "express";
 import {
+  createWorkExperienceController,
   deleteWorkExperienceController,
   getAllWorkExperienceController,
-  getOneWorkExperienceController
+  getOneWorkExperienceController,
 } from "../../../controllers/work-experience";
 import { UuidDTO } from "../../../dto/common.dto";
 import { validateDTO } from "../../../middlewares/dto/validate-dto.middleware";
+import { CreateWorkExperienceDTO } from "./../../../dto/work-experience.dto";
 
 const router = Router();
 
-router.delete("/:uuid", validateDTO(UuidDTO, "params"), deleteWorkExperienceController);
+router.post(
+  "/",
+  validateDTO(CreateWorkExperienceDTO),
+  createWorkExperienceController
+);
+router.delete(
+  "/:uuid",
+  validateDTO(UuidDTO, "params"),
+  deleteWorkExperienceController
+);
 router.get("/", getAllWorkExperienceController);
-router.get("/:uuid", validateDTO(UuidDTO, "params"), getOneWorkExperienceController);
+router.get(
+  "/:uuid",
+  validateDTO(UuidDTO, "params"),
+  getOneWorkExperienceController
+);
+router.patch(
+  "/:uuid",
+  [validateDTO(UuidDTO, "params"), validateDTO(CreateWorkExperienceDTO)],
+  getOneWorkExperienceController
+);
 
 export default router;
