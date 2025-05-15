edc0ede8e904c4949a89bccd37e71227f3fe55a4
TommyGC11 <tommycontreras34@gmail.com>
2025-05-15 16:38:08 -0400

    Enhance getAllCandidateController to include desiredPosition, department, and workExperience relations in the response

--- src/controllers/candidate/getAll.controller.ts
+++ src/controllers/candidate/getAll.controller.ts
@@ -3,11 +3,29 @@ import { getAllCandidateService } from "../../services/candidate/getAll.service"
 import { statusCode } from "../../utils/status.util";
 
 export const getAllCandidateController = async (_req: Request, res: Response) => {
-  getAllCandidateService({})
+  getAllCandidateService({
+    relations: {
+      desiredPosition: true,
+      department: true,
+      workExperience: true
+    }
+  })
     .then((data) => {
       const candidates = data.map((candidate) => ({
         uuid: candidate.uuid,
         name: candidate.name,
+        desiredPosition: {
+          uuid: candidate.desiredPosition.uuid,
+          name: candidate.desiredPosition.name,
+        },
+        department: {
+          uuid: candidate.department.uuid,
+          name: candidate.department.name,
+        },
+        workExperience: {
+          uuid: candidate.workExperience.uuid,
+          company: candidate.workExperience.company
+        },
         status: candidate.status,
       }));
 
