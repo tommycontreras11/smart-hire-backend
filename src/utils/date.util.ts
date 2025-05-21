0776fca3f9ab08eea6456b7e83a2f759dbfc0e03
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-05-20 21:36:00 -0400

    Enhance date utility functions and update user retrieval logic to include email validation

--- src/utils/date.util.ts
+++ src/utils/date.util.ts
@@ -9,6 +9,24 @@ export const getDaysBetweenDates = (startDate: string, endDate: string): number 
   return Math.ceil(diffInMs / (1000 * 60 * 60 * 24));
 };
 
+export const timeAgo = (date: Date): string => {
+  const now = new Date();
+  const diff = now.getTime() - date.getTime();
+
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(diff / (1000 * 60));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+  const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
+
+  if (years > 0) return `Hace ${years}a`;
+  if (months > 0) return `Hace ${months}m`;
+  if (days > 0) return `Hace ${days}d`;
+  if (hours > 0) return `Hace ${hours}h`;
+  if (minutes > 0) return `Hace ${minutes}min`;
+  return `Hace ${seconds}s`;
+}
 
 export const getFullDate = (date: Date) => {
   const year = date.getFullYear();
--- src/utils/user.util.ts
+++ src/utils/user.util.ts
@@ -5,19 +5,23 @@ import { statusCode } from "./status.util";
 
 export async function retrieveIfUserExists(
   identification?: string | null,
+  email?: string | null,
   uuid?: string | null
 ) {
   const entities = await Promise.all([
     CandidateEntity.findOneBy({
       ...(identification && { identification }),
+      ...(email && { email }),
       ...(uuid && { uuid }),
     }),
     EmployeeEntity.findOneBy({
       ...(identification && { identification }),
+      ...(email && { email }),
       ...(uuid && { uuid }),
     }),
     RecruiterEntity.findOneBy({
       ...(identification && { identification }),
+      ...(email && { email }),
       ...(uuid && { uuid }),
     }),
   ]);
@@ -25,14 +29,15 @@ export async function retrieveIfUserExists(
   return entities.find((entity) => entity) || null;
 }
 
-export async function validateIdentification<T>(entityClass: { new (): T }, identification: string) {
-    const foundUser = await retrieveIfUserExists(identification);
+export async function validateProperty<T>(entityClass: { new (): T }, property: string, columnName: "Identification" | "Email") {
+    const propertyToValidate = columnName === "Identification" ? true : false;
+    const foundUser = await retrieveIfUserExists(propertyToValidate ? property : null, property);
   
     const isEntityClassType = foundUser instanceof entityClass;
   
     if (foundUser && (!isEntityClassType || isEntityClassType)) {
       return Promise.reject({
-        message: "Identification already exists",
+        message: `${columnName} already exists`,
         status: statusCode.BAD_REQUEST,
       });
     }
