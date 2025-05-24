25b7f6316d53cb5b84baea9ad04ddd77eb910295
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-05-23 22:23:44 -0400

    Enhance competency controllers to include relations for category, evaluationMethods, and positionTypes

--- src/controllers/competency/getAll.controller.ts
+++ src/controllers/competency/getAll.controller.ts
@@ -3,11 +3,30 @@ import { getAllCompetencyService } from "../../services/competency/getAll.servic
 import { statusCode } from "../../utils/status.util";
 
 export const getAllCompetencyController = async (_req: Request, res: Response) => {
-  getAllCompetencyService({})
+  getAllCompetencyService({
+    relations: {
+      category: true,
+      evaluationMethods: true,
+      positionTypes: true,
+    }
+  })
     .then((data) => {
       const competencies = data.map((competency) => ({
         uuid: competency.uuid,
         name: competency.name,
+        category: {
+          uuid: competency.category.uuid,
+          name: competency.category.name,
+        },
+        level: competency.level,
+        evaluationMethods: competency.evaluationMethods.map((evaluationMethod) => ({
+          uuid: evaluationMethod.uuid,
+          name: evaluationMethod.name,
+        })),
+        positionTypes: competency.positionTypes.map((positionType) => ({
+          uuid: positionType.uuid,
+          name: positionType.name,
+        })),
         status: competency.status,
       }));
 
--- src/controllers/competency/getOne.controller.ts
+++ src/controllers/competency/getOne.controller.ts
@@ -2,18 +2,39 @@ import { Request, Response } from "express";
 import { getOneCompetencyService } from "../../services/competency/getOne.service";
 import { statusCode } from "../../utils/status.util";
 
-export const getOneCompetencyController = async (req: Request, res: Response) => {
+export const getOneCompetencyController = async (
+  req: Request,
+  res: Response
+) => {
   const { uuid } = req.params;
 
   getOneCompetencyService({
     where: {
       uuid,
     },
+    relations: {
+      category: true,
+      evaluationMethods: true,
+      positionTypes: true,
+    },
   })
     .then((data) => {
       const competency = {
         uuid: data.uuid,
         name: data.name,
+        category: {
+          uuid: data.category.uuid,
+          name: data.category.name,
+        },
+        level: data.level,
+        evaluationMethods: data.evaluationMethods.map((evaluationMethod) => ({
+          uuid: evaluationMethod.uuid,
+          name: evaluationMethod.name,
+        })),
+        positionTypes: data.positionTypes.map((positionType) => ({
+          uuid: positionType.uuid,
+          name: positionType.name,
+        })),
         status: data.status,
       };
 
