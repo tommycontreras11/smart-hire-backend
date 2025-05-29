387b03be5f98717c7ec547627170717654281e63
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-05-28 21:30:04 -0400

    feat: add MinIO service to docker-compose; configure object storage in the application

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { connectDatabase } from "./database/connection"
-import app from "./app"
+import { connectDatabase } from "./database/connection";
+import app from "./app";
+import { ObjectStorage } from "./libs/object-storage";
 
 async function main() {
-    await connectDatabase()
-    app.listen(process.env.APP_PORT || 3000)
-    console.log(`Server running on port ${process.env.APP_PORT || 3000} 🚀`)
+  await connectDatabase();
+  await ObjectStorage.instance.setConfiguration();
+  app.listen(process.env.APP_PORT || 3000);
+  console.log(`Server running on port ${process.env.APP_PORT || 3000} 🚀`);
 }
 
-main()
+main();
