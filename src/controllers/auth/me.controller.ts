bd6ba63a781aa12458357cb9f751a41401defd8d
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-06-28 10:02:22 -0400

    feat: update meController to handle photo retrieval asynchronously from ObjectStorage

--- src/controllers/auth/me.controller.ts
+++ src/controllers/auth/me.controller.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from "express";
 import { meService } from "./../../services/auth/me.service";
 import { statusCode } from "./../../utils/status.util";
+import { ObjectStorage } from "./../../libs/object-storage";
 
 export const meController = async (req: Request, res: Response) => {
   meService(req?.user?.uuid)
-    .then(({ user, userType }) => {
+    .then(async ({ user, userType }) => {
+      const storage = ObjectStorage.instance
+
       const data = {
         uuid: user.uuid,
         identification: user.identification,
         email: user.email,
         name: user.name,
         role: userType,
+        ...(user.photo && {
+          photo: await storage.getUrl(user.photo)
+        })
       };
 
       return res.status(statusCode.OK).json({ data });
